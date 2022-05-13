406e4cf323c111aa468176af99819a686318bd42
Xilosof <pro100temi444@gmail.com>
2022-05-13 17:36:42 +0300

    try to create weapon

--- Assets/Assets/Scripts/Managers/AttackManager.cs
+++ Assets/Assets/Scripts/Managers/AttackManager.cs
@@ -1,7 +1,15 @@
+using Assembly_CSharp.Assets.Assets.Scripts.Interfaces;
+
 namespace Assembly_CSharp.Assets.Assets.Scripts.Managers
 {
-	public static class AttackManager
-	{
-	 
-	}
+    public class AttackManager
+    {
+        public static void Attack(ICanAttack entity)
+        {
+			if (entity.IsCanAttack)
+			{
+				entity.
+			}
+        }
+    }
 }
