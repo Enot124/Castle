fc77f9a0e5a8dc45bcc495883ebdde408d0e9802
Enot <dan.enot@gmail.com>
2022-08-19 14:56:37 +0300

    Add Screenshots

--- Assets/Assets/Scripts/Controllers/KeyboardController.cs
+++ Assets/Assets/Scripts/Controllers/KeyboardController.cs
@@ -4,31 +4,31 @@ using UnityEngine.UIElements;
 
 namespace Assembly_CSharp.Assets.Assets.Scripts.Controllers
 {
-    public class KeyboardController : MonoBehaviour
-    {
-        [SerializeField] private Hero _hero;
-        void Update()
-        {
-            if (Input.GetMouseButtonDown(0))
-            {
-                AttackManager.Attack(_hero);
-            }
-        }
-        
-        void FixedUpdate()
-        {
-            Vector2 dir;
+   public class KeyboardController : MonoBehaviour
+   {
+      [SerializeField] private Hero _hero;
+      void Update()
+      {
+         if (Input.GetMouseButtonDown(0))
+         {
+            AttackManager.Attack(_hero);
+         }
+      }
 
-            dir.x = Input.GetAxisRaw("Horizontal");
-            dir.y = Input.GetAxisRaw("Vertical");
+      void FixedUpdate()
+      {
+         Vector2 dir;
 
-            //dir.Normalize();
-            if (dir.x != 0 || dir.y != 0)
-                MoveManager.Move(_hero, dir * Time.fixedDeltaTime);
-            else
-                MoveManager.Stop(_hero);
+         dir.x = Input.GetAxisRaw("Horizontal");
+         dir.y = Input.GetAxisRaw("Vertical");
+
+         //dir.Normalize();
+         if (dir.x != 0 || dir.y != 0)
+            MoveManager.Move(_hero, dir * Time.fixedDeltaTime);
+         else
+            MoveManager.Stop(_hero);
 
 
-        }
-    }
+      }
+   }
 }
